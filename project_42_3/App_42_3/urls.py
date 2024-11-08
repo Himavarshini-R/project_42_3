f3d3243abe505886d7b780826f9e89ebf48fc27c
Himavarshini-R <himavarshini.ramachandruni@gmail.com>
2024-11-08 23:13:48 +0530

    first commit

--- project_42_3/App_42_3/urls.py
+++ project_42_3/App_42_3/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('achievements/', views.achievements, name='achievements'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
 ]
--- project_42_3/App_42_3/views.py
+++ project_42_3/App_42_3/views.py
@@ -1,20 +1,14 @@
+# profile/views.py
 from django.shortcuts import render
-from .models import About, PortfolioItem
 
 def home(request):
-    about = About.objects.first()  # Get the first entry, if any
-    if not about:
-        # Fallback if no About object exists
-        about = {
-            'name': 'Your Name',
-            'bio': 'Bio not available yet.',
-            'email': 'your.email@example.com',
-            'profile_picture': 'default_profile_pic.jpg',  # Replace with a default image path
-        }
-    
-    portfolio_items = PortfolioItem.objects.all()
-    
-    return render(request, 'home.html', {'about': about, 'portfolio_items': portfolio_items})
+    return render(request, 'home.html')
 
+def achievements(request):
+    return render(request, 'achievements.html')
 
+def projects(request):
+    return render(request, 'projects.html')
 
+def contact(request):
+    return render(request, 'contact.html')
